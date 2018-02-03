5954ced2aca8623fad6a3f9bf5f4daebee6220ad
Charlie Blackstock <charlieblackstock@gmail.com>
2018-02-03 11:06:47 -0700

    add lottery contract and tests

---
+++ solidity-lottery/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./compile')
+const mnemonic = require('./config')
+
+const provider = new HDWalletProvider(
+  mnemonic,
+  'https://rinkeby.infura.io/wDLKFRfVVGvZRWJ8Dugi'
+)
+
+const web3 = new Web3(provider)
+
+async function deploy() {
+  const accounts = await web3.eth.getAccounts()
+  console.log('Attempting to deploy from account', accounts[0])
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: '1000000' })
+
+  console.log('Contract deployed to', result.options.address)
+}
+deploy()
